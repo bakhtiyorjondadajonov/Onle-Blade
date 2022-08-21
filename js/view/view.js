1d8df8a825c5fc3526485f0f7881f5c325659531
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-22 02:32:54 +0900

    pdp header done

--- js/view/view.js
+++ js/view/view.js
@@ -1,10 +1,23 @@
 // ----------- RADIO BUTTONS FUNCTIONALITY -------------
 export const a = "a";
-const window = document.querySelector("body");
-window.addEventListener("click", function (e) {
-  e.preventDefault();
-  const btn = e.target.closest(".label");
-  if (!btn) return;
-  console.log(btn);
-  btn.style.border = "solid 1px #BEBEBE ";
+
+// --------------- DROP DOWN FUNCTUALITY --------------//
+const selectField = document.querySelector(".select-field");
+const dropdownList = document.querySelector(".drop-down__list");
+
+selectField.addEventListener("click", function (e) {
+  dropdownList.classList.toggle("hidden");
+});
+dropdownList.addEventListener("click", function (e) {
+  const content = e.target.closest(".option").innerHTML;
+  console.log("content: ", content);
+  if (!content) return;
+  const html = `
+  ${content}<div class="selector__img-box">
+  <img src="../public/icons/arrow.png" alt="" class="selector__img">
+
+</div>
+  `;
+  selectField.innerHTML = html;
+  dropdownList.classList.add("hidden");
 });
