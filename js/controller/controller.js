fa5701a9bf0d4f65da1c58c8526d5d0315d6a4dc
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-22 16:44:11 +0900

    pdp part is done

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,1 +1,12 @@
 import { a } from "../view/view.js";
+
+const elem = document.querySelector(".main-carousel");
+if (elem) {
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    contain: true,
+    pageDots: false,
+    //   dots: false,
+  });
+}
--- js/view/view.js
+++ js/view/view.js
@@ -4,20 +4,35 @@ export const a = "a";
 // --------------- DROP DOWN FUNCTUALITY --------------//
 const selectField = document.querySelector(".select-field");
 const dropdownList = document.querySelector(".drop-down__list");
+if (selectField && dropdownList) {
+  selectField.addEventListener("click", function (e) {
+    dropdownList.classList.toggle("hidden");
+  });
+  dropdownList.addEventListener("click", function (e) {
+    const content = e.target.closest(".option").innerHTML;
+    console.log("content: ", content);
+    if (!content) return;
+    const html = `
+    ${content}<div class="selector__img-box">
+    <img src="../public/icons/arrow.png" alt="" class="selector__img">
+  
+  </div>
+    `;
+    selectField.innerHTML = html;
+    dropdownList.classList.add("hidden");
+  });
+}
 
-selectField.addEventListener("click", function (e) {
-  dropdownList.classList.toggle("hidden");
-});
-dropdownList.addEventListener("click", function (e) {
-  const content = e.target.closest(".option").innerHTML;
-  console.log("content: ", content);
-  if (!content) return;
-  const html = `
-  ${content}<div class="selector__img-box">
-  <img src="../public/icons/arrow.png" alt="" class="selector__img">
-
-</div>
-  `;
-  selectField.innerHTML = html;
-  dropdownList.classList.add("hidden");
+// ------------- RADIO BUTTONS FUNCTUANLITY --------------
+const window = document.querySelector("body");
+console.log("window: ", window);
+window.addEventListener("click", function (e) {
+  const btn = e.target.closest(".checkbox");
+  if (!btn || btn.classList.contains("checkbox--disabled")) return;
+  const swatchesBox = btn.parentElement.parentElement;
+  const allLabels = document.querySelectorAll(".checkbox-label");
+  allLabels.forEach((eachLabel) =>
+    eachLabel.classList.remove("checkbox-label--checked")
+  );
+  btn.parentElement.classList.add("checkbox-label--checked");
 });
