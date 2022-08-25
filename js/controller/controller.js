55d6bf48367230ee13b45d2f827071252849f811
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-25 09:57:24 +0900

    finished project

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,12 +1,30 @@
 import { a } from "../view/view.js";
 
 const elem = document.querySelector(".main-carousel");
+const btnLeft = document.querySelector(".arrow-left");
+const btnRight = document.querySelector(".arrow-right");
 if (elem) {
   const flkty = new Flickity(elem, {
     // options
     cellAlign: "left",
     contain: true,
     pageDots: true,
-    //   dots: false,
+    prevNextButtons: false,
+  });
+  btnLeft.addEventListener("click", function () {
+    flkty.previous(true);
+  });
+  // next
+  btnRight.addEventListener("click", function () {
+    flkty.next(true);
+  });
+}
+
+// ADVERTISEMENT -------------//
+const adContainer = document.querySelector(".advertisement");
+const adBtn = document.querySelector(".avertisement__close-icon");
+if (adBtn) {
+  adBtn.addEventListener("click", function (e) {
+    adContainer.classList.add("hidden");
   });
 }
--- js/view/view.js
+++ js/view/view.js
@@ -1,4 +1,3 @@
-// ----------- RADIO BUTTONS FUNCTIONALITY -------------
 export const a = "a";
 
 // --------------- DROP DOWN FUNCTUALITY --------------//
@@ -25,14 +24,44 @@ if (selectField && dropdownList) {
 
 // ------------- RADIO BUTTONS FUNCTUANLITY --------------
 const window = document.querySelector("body");
-console.log("window: ", window);
+
 window.addEventListener("click", function (e) {
   const btn = e.target.closest(".checkbox");
   if (!btn || btn.classList.contains("checkbox--disabled")) return;
   const swatchesBox = btn.parentElement.parentElement;
-  const allLabels = document.querySelectorAll(".checkbox-label");
+  const allLabels = swatchesBox.querySelectorAll(".checkbox-label");
   allLabels.forEach((eachLabel) =>
     eachLabel.classList.remove("checkbox-label--checked")
   );
   btn.parentElement.classList.add("checkbox-label--checked");
 });
+window.addEventListener("click", function (e) {
+  const btn = e.target.closest(".checkbox--pdp");
+  if (!btn) return;
+  const swatchesBox = btn.parentElement.parentElement;
+  const allLabels = swatchesBox.querySelectorAll(".checkbox");
+  allLabels.forEach((eachLabel) =>
+    eachLabel.classList.remove("checkbox--black")
+  );
+  btn.classList.add("checkbox--black");
+});
+
+// ------------ CART POPUP ----------------//
+
+const cartBtn = document.querySelector(".cart-button");
+const cart = document.querySelector(".content-container");
+const cartCloseBtn = document.querySelector(".cart-close_btn");
+if (cartBtn) {
+  cartBtn.addEventListener("click", function (e) {
+    cart.classList.remove("cart-close");
+    cart.classList.remove("hidden");
+  });
+}
+if (cartCloseBtn) {
+  cartCloseBtn.addEventListener("click", function (e) {
+    cart.classList.add("cart-close");
+  });
+  setTimeout(() => {
+    cart.classList.add("hidden");
+  }, 300);
+}
